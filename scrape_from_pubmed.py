69834859c6c256551da676f39889fc7b41967fdd
kaicheng liang <kliang933@gmail.com>
2019-04-22 22:22:11 +0800

    replace parser with minidom, write titles to csv file

--- scrape_from_pubmed.py
+++ scrape_from_pubmed.py
@@ -14,13 +14,14 @@ Created on Thu Mar 28 13:50:39 2019
 # https://marcobonzanini.wordpress.com/2015/01/12/searching-pubmed-with-python/
 
 from Bio import Entrez
-from bs4 import BeautifulSoup
+from xml.dom import minidom
+import csv
 
 def search(query):
 	Entrez.email = 'your.email@example.com'
-	handle = Entrez.esearch(db='pmc', 
-							sort='relevance', 
-							retmax='2',
+	handle = Entrez.esearch(db='pubmed', 
+							sort='most+recent', 
+							retmax='100000',
 							retmode='xml', 
 							reldate = '3650',
 							term=query)
@@ -30,23 +31,29 @@ def search(query):
 def fetch_details(id_list):
 	ids = ','.join(id_list)
 	Entrez.email = 'your.email@example.com'
-	handle = Entrez.efetch(db='pmc',
-						   retmode='xml',
-						   id=ids)
-	results=BeautifulSoup(handle, features="xml")
-	return results
+	handle = Entrez.efetch(db='pubmed',
+                           retmode='xml',
+                           id=ids)
+	#results = Entrez.read(handle)
+	return handle
 
 if __name__ == '__main__':
-	results = search('optical coherence tomography')
-	id_list = results['IdList']
-	papers = fetch_details(id_list)
-	for paper in papers.findAll('mixed-citation'):
-		pmcid = dict(paper.attrs)
-		try:
-			print(paper.find('article-title').contents)
-		except:
-			print('no article title')
-		#print("%d) %s" % (i+1, paper['MedlineCitation']['Article']['ArticleTitle']))
-	# Pretty print the first paper in full
-	#import json
-#print(json.dumps(papers[0], indent=2, separators=(',', ':')))
+
+	with open('paper-titles-data.csv', mode='w') as data_file:
+		file_writer =  csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		results = search('biomedical optics')
+		id_list = results['IdList']
+		papers = fetch_details(id_list)
+		data = minidom.parse(papers)
+		for title in data.getElementsByTagName("ArticleTitle"):
+			for node in title.childNodes:
+				if node.nodeType == node.TEXT_NODE:
+					print(node.data)
+					try:
+						file_writer.writerow([node.data, '1'])
+					except:
+							pass
+	data_file.close()
+#		for i, paper in enumerate(papers['PubmedArticle']):
+#			#file_writer.writerow([paper['MedlineCitation']['Article']['ArticleTitle'], '1'])
+#			print("%d) %s" % (i+1, paper['MedlineCitation']['Article']['ArticleTitle']))
