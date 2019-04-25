885ac3066367b2a0fd3af6c8505e59e6e987c3a8
kaicheng liang <kliang933@gmail.com>
2019-04-25 22:37:29 +0800

    credentials

---
+++ credentials.py
@@ -0,0 +1,7 @@
+consumer_key = 'PLp7DQOb165ju6ELh3yCe8Jaa'
+consumer_secret = '91f31iX7SyDTFuo0JX33QSfwcVtkqmdXJHfoY5vDgUo3ceA3M9'
+access_token = '1121230191718649856-5RqmDAuWddzXVKsUuhRgkNhdbvzRZb'
+access_token_secret = '7Egp8HQAjlGSud7c8wNlOMPPYltCUuECpFqxF0dMK7OzE'
+
+API_USER = "o_271hv0c0ud"
+API_KEY = "R_57bcb07dd2a59e4b26deb76d6abfbe84"
