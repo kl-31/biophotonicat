5e6abd17e84d1e1d65cdd69f4fd8a14f61edaac9
kaicheng liang <kliang933@gmail.com>
2019-06-05 06:53:55 +0800

    add nanoscopy to training model

--- scrape_from_pubmed_arxiv.py
+++ scrape_from_pubmed_arxiv.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 	# PUBMED QUERY
 	groups = [['physics','engineering','cell biology','medical imaging'],
 			   ['biomedical optics express', 'journal of biophotonics','journal of biomedical optics', 'optical imaging','biophotonics','biomedical optics',
-	   'optical coherence tomography','photoacoustic tomography','nonlinear microscopy','super-resolution microscopy','optical biopsy']]
+	   'optical coherence tomography','photoacoustic tomography','nonlinear microscopy','super-resolution microscopy','optical biopsy','nanoscopy']]
 	start = time.time()
 	with open('new-paper-titles-data-abstracts3.csv', mode='w') as data_file:
 		file_writer =  csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
